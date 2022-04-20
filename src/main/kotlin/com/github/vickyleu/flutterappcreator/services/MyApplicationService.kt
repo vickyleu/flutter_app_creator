0cd66a26e230242b6406f223f5d87856e98a1789
GitHub Action <action@github.com>
2022-04-20 10:35:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vickyleu/flutterappcreator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vickyleu.flutterappcreator.services
+
+import com.github.vickyleu.flutterappcreator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/vickyleu/flutterappcreator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vickyleu/flutterappcreator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vickyleu.flutterappcreator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vickyleu.flutterappcreator.MyBundle
 
 class MyProjectService(project: Project) {
 
